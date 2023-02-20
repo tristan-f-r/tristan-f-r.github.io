cf3edeac7fec0df24a1e8b2fc3e6284a3befae4d
Tristan F <LeoDog896@hotmail.com>
2023-02-20 16:32:06 -0500

    fix: jekyll url on webpage

--- grab.ts
+++ grab.ts
@@ -31,7 +31,7 @@ let markdown = `# leodog896.github.io
 
 these are auto-generated lists of repositories on my account, mainly for catalogue info.
 
-looking for my website? go to https://leodog896.com instead.
+looking for my website? go to [https://leodog896.com](https://leodog896.com) instead.
 
 ## Repositories
 `;
